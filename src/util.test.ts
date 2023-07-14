de0a6555dde9995f88a74a27c4260eb6fff1fde1
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2023-07-14 16:58:29 +0200

    test: inline script render

--- src/util.test.ts
+++ src/util.test.ts
@@ -2,8 +2,14 @@ import {getRenderHelpers} from './utils.js';
 
 test('should not render nonce for regular `<script>` tags', () => {
     const script = getRenderHelpers({nonce: 'random'}).renderScript({
-        src: 'alert(1)',
+        src: 'foo.js',
     });
 
     expect(script).not.toEqual(expect.stringContaining('nonce="random"'));
+});
+
+test('should render `nonce` for inline `<script>` tags', () => {
+    const script = getRenderHelpers({nonce: 'random'}).renderInlineScript('alert(1)');
+
+    expect(script).toEqual(expect.stringContaining('nonce="random"'));
 });
