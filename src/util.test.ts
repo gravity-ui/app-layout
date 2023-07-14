f1fe0c2dc0b32b1bae126799b914826de69e57ef
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2023-07-14 17:24:22 +0200

    test: inline styles render

--- src/util.test.ts
+++ src/util.test.ts
@@ -1,7 +1,13 @@
 import {getRenderHelpers} from './utils.js';
 
+let helpers: ReturnType<typeof getRenderHelpers>;
+
+beforeEach(() => {
+    helpers = getRenderHelpers({nonce: 'random'});
+});
+
 test('should not render nonce for regular `<script>` tags', () => {
-    const script = getRenderHelpers({nonce: 'random'}).renderScript({
+    const script = helpers.renderScript({
         src: 'foo.js',
     });
 
@@ -9,7 +15,13 @@ test('should not render nonce for regular `<script>` tags', () => {
 });
 
 test('should render `nonce` for inline `<script>` tags', () => {
-    const script = getRenderHelpers({nonce: 'random'}).renderInlineScript('alert(1)');
+    const script = helpers.renderInlineScript('alert(1)');
 
     expect(script).toEqual(expect.stringContaining('nonce="random"'));
+});
+
+test('should render `nonce` for inline styles', () => {
+    const style = helpers.renderInlineStyle('/* foobar */');
+
+    expect(style).toEqual(expect.stringContaining('nonce="random"'));
 });
