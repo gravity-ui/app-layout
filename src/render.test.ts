1db127a35b4116e46ded8ba909436e8644a6e128
Valerii Sidorenko <balepas@yandex-team.ru>
2023-08-16 01:24:37 +0200

    fix: render empty root content if bodyContent.root is not set (#18)

--- src/render.test.ts
+++ src/render.test.ts
@@ -14,3 +14,10 @@ test('should allow `<html>` attributes override', () => {
     expect(createRenderFunction()({title: 'Foobar'})).toMatch('<html >');
     expect(createRenderFunction([dirPlugin()])({title: 'Foobar'})).toMatch('<html dir="ltr">');
 });
+
+test('should render root content', () => {
+    expect(createRenderFunction()({title: 'Foobar'})).toMatch(/<div id="root">\s*<\/div>/);
+    expect(createRenderFunction()({title: 'Foobar', bodyContent: {root: 'content'}})).toMatch(
+        /<div id="root">\s*content\s*<\/div>/,
+    );
+});
