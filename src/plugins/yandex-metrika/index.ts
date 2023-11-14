f5c0b1c2e9e73607470ae05049dfdd2bc007e2b0
Nikita Gorin <36075690+NikitaCG@users.noreply.github.com>
2023-11-14 16:56:59 +0300

    Update src/plugins/yandex-metrika/index.ts
    commit suggestion
    
    Co-authored-by: Valerii Sidorenko <balepas@yandex-team.ru>

--- src/plugins/yandex-metrika/index.ts
+++ src/plugins/yandex-metrika/index.ts
@@ -73,7 +73,7 @@ export function renderMetrika(params: MetrikaPluginOptions, utils: RenderHelpers
                     ecommerce: ${config.ecommerce},
                     type: ${Number(config.type)},
                     experiments: "${config.encryptedExperiments || ''}"
-                    userParams: "${config?.userParams || {}}"
+                    userParams: ${JSON.stringify(config?.userParams || {})}
                 });
             `,
                 )
