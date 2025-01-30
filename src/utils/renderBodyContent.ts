56175321c72d3323e3fb102d628bd02288718a40
Alexander Glushenko <pistch@yandex.ru>
2025-01-30 18:58:31 +0300

    fix(RenderBodyContent): make renderBodyContent ssr-friendly

--- src/utils/renderBodyContent.ts
+++ src/utils/renderBodyContent.ts
@@ -3,9 +3,7 @@ import type {BodyContent} from '../types.js';
 export function renderBodyContent(content: BodyContent): string {
     return `
         ${content.beforeRoot.join('\n')}
-        <div id="root">
-            ${content.root ?? ''}
-        </div>
+        <div id="root">${content.root ?? ''}</div>
         ${content.afterRoot.join('\n')}
     `.trim();
 }
